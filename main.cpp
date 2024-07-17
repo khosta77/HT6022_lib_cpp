1e0e6b76d426f0d88b6ee85084d02cf95beb4fa2
khosta77 <puwerfulpants@gmail.com>
2024-07-17 14:26:44 +0300

    Смотрел графики из gnuplot

--- main.cpp
+++ main.cpp
@@ -1,35 +1,59 @@
+
+
+#include "ht6022lib.h"
 #include "gnuplot.h"
 
-int main(){
+void plotgnuplot()
+{
     GnuplotPipe gp;
-	gp.sendLine("set terminal png");
+	gp.sendLine("set title 'Line'");
+	gp.sendLine("set xlabel 'x'");
+	gp.sendLine("set ylabel 'y'");
+
+	gp.sendLine("set grid");
+	gp.sendLine("set key below center horizontal noreverse enhanced autotitle box dashtype solid");
+	gp.sendLine("set tics out nomirror");
+	gp.sendLine("set border 3 front linetype black linewidth 1.0 dashtype solid");
+
+	gp.sendLine("set yrange [-127:127]");
+	//gp.sendLine("set xrange [7000:9000]");
+	//gp.sendLine("set xtics 1, .5, 5");
+	//gp.sendLine("set mxtics 1");
+	gp.sendLine("set style line 1 linecolor rgb '#0060ad' linetype 1 linewidth 1");
+
+	gp.sendLine("set terminal pngcairo enhanced font 'Arial,12' size 1600,1200");
 	gp.sendLine("set output 'output.png'");
-    gp.sendLine("plot [-pi/2:pi] cos(x),-(sin(x) > sin(x+1) ? sin(x) : sin(x+1))");
-    return 0;
+	gp.sendLine("plot './signal_CH1.txt' using 1:2 with lines linestyle 1 title 'data',\\");
+	gp.sendLine("	'' using 1:3 with lines linestyle 2 title 'line2'");
 }
-#if 0
-#include "ht6022lib.h"
-//#include <set>
-//#include <matplot/matplot.h>
 
 int main()
 {
-#if 0
     oscilloscopes::hantek::ht6022be osc;
     std::ofstream out_ch1("signal_CH1.txt"), out_ch2("signal_CH2.txt"); 
-    for( size_t i = 0; i < 10; ++i )
+    for( size_t i = 0, j = 0; i < 3; ++i )
     {
-        auto signal = osc.readFrame(oscilloscopes::hantek::HT6022_1KB, 0.914306640625, 9.122314453125);
+        auto signal = osc.readFrame(oscilloscopes::hantek::HT6022_1MB, 0.914306640625, 9.122314453125);
 
-        for( auto it : signal.first )
-            out_ch1 << ((int)it) << ' ';
-        for( auto it : signal.second )
-            out_ch2 << ((int)it) << ' ';
+
+        for( size_t k = 0; k < signal.first.size(); ++k, ++j )
+		{
+            out_ch1 << ( j ) << ' ' << ((int)signal.first[k]);
+			if( k > ( signal.first.size() - 3 ) )
+			{
+				out_ch1 << ' ' << -127;
+				std::cout << j << std::endl;
+			}
+			else
+				out_ch1 << ' ' << 1000;
+			out_ch1 << std::endl;
+		}
+		//for( auto it : signal.second )
+        //    out_ch2 << ((int)it) << ' ';
     }
     out_ch1.close();
     out_ch2.close();
-#endif
+	plotgnuplot();
     return 0;
 }
-#endif
 
