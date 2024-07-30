9effbb0dba1ea07ad08c0a15a01c09176facf621
khosta77 <puwerfulpants@gmail.com>
2024-07-30 22:13:28 +0300

    Выкинул все, что не связано напрямую с осциллографом

--- project/main.cpp
+++ project/main.cpp
@@ -1,12 +1,8 @@
 #include "hantek6022.h"
-//#include "gnuplot.h"
 #include <algorithm>
 #include <iomanip>
 #include <thread>
 #include <atomic>
-#ifdef RASPBERRY_PI
-#include <wiringPi.h>
-#endif
 
 #define CH1_CALIBRATION 0.914306640
 #define CH2_CALIBRATION 9.245046616
@@ -16,85 +12,33 @@ using HT6022 = oscilloscopes::hantek::Hantek6022;
 using oscilloscopes::OscSigframe;
 using oscilloscopes::OscSignal;
 
-std::atomic<bool> done(false);
 
-class Moderator
+
+OscSigframe downSignal( OscSigframe osf, const size_t& channelSize, const int& down = 127,
+                        const std::vector<float>& CHx_CALIBRATION = { CH1_CALIBRATION,
+                        CH2_CALIBRATION } )
 {
-private:
-    oscilloscopes::Oscilloscope *_oscilloscope;
-    const size_t _memory;
-
-    OscSigframe read( const size_t& Memory )
+    for( size_t i = 0; i < channelSize; ++i )
     {
-        return _oscilloscope->getSignalFrame(Memory);
+        for( size_t j = 0; j < osf[i]._signalSize; ++j )
+            osf[i]._signal[j] -= ( down + CHx_CALIBRATION[i] );
     }
+    return osf;
+}
 
-    OscSigframe downSignal( OscSigframe osf, const size_t& channelSize, const int& down = 127,
-                            const std::vector<float>& CHx_CALIBRATION = { CH1_CALIBRATION,
-                            CH2_CALIBRATION } )
-    {
-        for( size_t i = 0; i < channelSize; ++i )
-        {
-            for( size_t j = 0; j < osf[i]._signalSize; ++j )
-                osf[i]._signal[j] -= ( down + CHx_CALIBRATION[i] );
-        }
-        return osf;
-    }
-
-    /** @brief Среднее значение сигнала
-     * */
-    double mean( const OscSignal& signal )
-    {
-        double avg = 0.0;
-        for( auto& it : signal._signal )
-            avg += it;
-        return ( avg / signal._signalSize );
-    }
-
-    static void delay( const long long int& time )
-    {
-        std::this_thread::sleep_for(std::chrono::nanoseconds(time));
-        done = true;
-    }
-public:
-    Moderator( oscilloscopes::Oscilloscope *osc, const size_t& mem ) : _oscilloscope(osc), _memory(mem)
-    {
-#ifdef RASPBERRY_PI
-        wiringPiSetup();
-        pinMode(26, OUTPUT);
-        digitalWrite(26, LOW);
-        pinMode(27, OUTPUT);
-        digitalWrite(27, LOW);
-#endif
-    }
-    ~Moderator() {}
-
-    void run( [[ maybe_unused ]] const long long int& wait )
-    {
-        
-        std::thread t( delay, 5'000'000'000 );
-
-#ifdef RASPBERRY_PI
-        digitalWrite(26, HIGH);
-#endif
-        t.join();
-        while(!done){}
-
-#ifdef RASPBERRY_PI
-        digitalWrite(27, HIGH);
-#endif
-
-        auto df = read(_memory);
-        df = downSignal( df, 2 ); 
-        std::cout << df[0]._signalSize << std::endl;
-    }
-};
+double mean( const OscSignal& signal )
+{
+    double avg = 0.0;
+    for( auto& it : signal._signal )
+        avg += it;
+    return ( avg / signal._signalSize );
+}
 
 int main()
 {
-    Hantek6022 A;
-    Moderator moderator( &A, HT6022::_1MB );
-    moderator.run(5'000'000'000);
+    Hantek6022 oscilloscope;
+
+
     return 0;
 }
 
